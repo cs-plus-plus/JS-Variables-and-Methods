c03cbb8554f72759d18858ee1c3967a705b6e3e1
CS++ <kevin@csplusplus.com>
2025-10-27 17:21:17 +0000

    init commit

--- script.js
+++ script.js
@@ -3,24 +3,16 @@
 
 function convertTemp() {
   // Ask for temperature in Fahrenheit
-  const fStr = prompt("Enter temperature in Fahrenheit");
-  const fVal = parseFloat(fStr);
-
+ 
   // Convert to Celsius
-  const cVal = (fVal - 32) * 5 / 9;
 
   // Display result rounded to two decimals
-  alert("Temperature in Celsius: " + cVal.toFixed(2));
 }
-
 function circleArea() {
   // Ask for circle radius
-  const rStr = prompt("Enter circle radius");
-  const rVal = parseFloat(rStr);
 
   // Calculate area
-  const area = Math.PI * rVal * rVal;
+ 
 
   // Display result rounded to two decimals
-  alert("Area of the circle: " + area.toFixed(2));
 }
